d0c27aa582e5071d6a19334a65b0d926f69d7cf4
Jake Campa and Patrick McGreevy <patrick@>
2017-03-29 10:42:51 -0700

    Start keg edit form

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,14 +15,27 @@ import { Component } from '@angular/core';
       </tr>
     </thead>
     <tbody>
-      <tr *ngFor="let keg of kegs">
+      <tr *ngFor="let keg of kegs" (click)='editKeg(keg)'>
         <td>{{keg.name}}</td>
         <td>{{keg.brand}}</td>
-        <td>$ {{keg.price}}</td>
+        <td> {{keg.price}}</td>
         <td>{{keg.abv}}</td>
       </tr>
     </tbody>
   </table>
+  <button (click)='newKeg()'>New</button>
+
+  <div *ngIf="edittingKeg">
+    <h1>{{edittingKeg.name}}</h1>
+
+    <form>
+      <label for='name'>Name:</label>
+      <input name='name' type='text' [(ngModel)]="edittingKeg.name" placeholder='Beer name'>
+
+      <label for='brand'>Brand:</label>
+      <input name='brand' type='text' [(ngModel)]="edittingKeg.brand" placeholder='Brand'>
+    </form>
+  </div>
   `
 })
 
@@ -31,9 +44,22 @@ export class AppComponent {
     new Keg('Bud Lite', 'Budweiser', 2.50, 0.05),
     new Keg('Foo', 'Bar', 1.00, 0.40)
   ];
+
+  edittingKeg: Keg = null;
+
+  editKeg(keg: Keg) {
+    this.edittingKeg = keg;
+  }
+
+  newKeg() {
+    let newKeg: Keg = new Keg('', '', null, null);
+    this.editKeg(newKeg);
+    this.kegs.push(newKeg);
+  }
 }
 
 export class Keg {
+  public volumeRemaining: number = 124;
   constructor(
     public name: string,
     public brand: string,
