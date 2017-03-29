6c9b09f54d7d75cdc328293f99a45a74a0b6a35f
Jake Campa and Patrick McGreevy <patrick@>
2017-03-29 09:57:24 -0700

    Implement Keg model

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,9 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <h1>Hello Tap Room</h1>
+  <ul>
+    <li *ngFor="let keg of kegs">
+      {{keg.name}}
+    </li>
+  </ul>
   `
 })
 
 export class AppComponent {
+  kegs: Keg[] = [
+    new Keg('Bud Lite', 'Budweiser', 2.50, 0.05),
+    new Keg('Foo', 'Bar', 1.00, 0.40)
+  ];
+}
 
+export class Keg {
+  constructor(
+    public name: string,
+    public brand: string,
+    public price: number,
+    public abv: number) {
+
+  }
 }
